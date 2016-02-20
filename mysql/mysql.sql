7047917f2c6a33f4ebb62f25902d112dcc6677fe
gururajkharvi <gpkprivate@gmail.com>
2016-02-20 14:48:04 +0530

    updated

---
+++ mysql/mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE 'tbl_login' (
+  'email' varchar(255) NOT NULL,
+  'password' varchar(255) NOT NULL
+);
+
+INSERT INTO 'tbl_login' ('email', 'password') VALUES
+('gpkharvi@gmail.com', 'password');
